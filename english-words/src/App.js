dabddad111e5d81629baceb571984e5b5b07dcc0
Verona4321 <sverona432@yandex.ru>
2025-02-23 16:26:20 +0400

    structure

--- english-words/src/App.js
+++ english-words/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
     <Header />
-    {/* Здесь вы можете добавить другие компоненты или содержимое приложения */}
+    {}
     <main>
         <h2>Welcome to My App!</h2>
         <p>This is the main content area.</p>
